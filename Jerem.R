a899d137dfd2ecea2e4441a8566c632fdf724fa1
Jérémie TORTERAT <jeremiet@ispf.pf>
2026-07-09 12:28:53 -1000

    Commit 4

---
+++ Jerem.R
@@ -0,0 +1,17 @@
+Mon premier fichier
+sdfkjhsdf
+kjhezkjrze
+and
+
+git config --global user.email = "email@example.com"
+
+and it creates .gitconfig file in C:/Users/admin/.gitconfig with correct values for them.
+
+After running the above lines of code three times this is what I got in that file:
+  
+  [user]
+name = =
+  email = =
+  name = =
+  
+  Why isn't it working? I followed the official tutorial and I see that it works for other people on YouTube but not for me.
