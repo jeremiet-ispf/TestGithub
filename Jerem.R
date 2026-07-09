5664d0e914089ae7bd849fac9c15487676600083
Jérémie TORTERAT <jeremiet@ispf.pf>
2026-07-09 12:29:14 -1000

    Commit Calimero

--- Jerem.R
+++ Jerem.R
@@ -7,6 +7,8 @@ git config --global user.email = "email@example.com"
 
 and it creates .gitconfig file in C:/Users/admin/.gitconfig with correct values for them.
 
+Et moi alors ?
+
 After running the above lines of code three times this is what I got in that file:
   
   [user]
